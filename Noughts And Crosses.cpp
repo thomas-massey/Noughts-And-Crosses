d32eaf835950b58a9f0209f837ef67b26e5bd554
Leftmostchain21 <thomassey476@outlook.com>
2022-09-28 11:39:59 +0100

    Add project files.

---
+++ Noughts And Crosses.cpp
@@ -0,0 +1,15 @@
+// This is a program that plays noughts and crosses. It is a two player game. The first player is X and the second player is O. The first player to get three in a row wins. The game is played on a 3x3 grid. The grid is numbered as follows:
+// It will make use of OOP
+
+#include <iostream>
+
+class Game {
+	public:
+		// Create a board attribute
+		char board[3][3];
+};
+	
+int main()
+{
+    
+}
